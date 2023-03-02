2a95f5abe263cf173c85bef204caaf91fdcdfa4f
Kutuzov Dmitry <47922037+ArchieSW@users.noreply.github.com>
2023-03-02 19:24:59 +0300

    HW 1 (#1)
    implement hw1

---
+++ landscape-service/src/main/java/dev/archie/landscapeservice/LandscapeApplication.java
@@ -0,0 +1,13 @@
+package dev.archie.landscapeservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LandscapeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LandscapeApplication.class, args);
+	}
+
+}
