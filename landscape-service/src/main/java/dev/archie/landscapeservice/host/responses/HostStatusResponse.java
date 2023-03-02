bd427774ceedd34765e738871f9d205084f71911
ArchieSW <archie-woc@ya.ru>
2023-03-02 20:18:59 +0300

    remove unused lombok annotations

--- landscape-service/src/main/java/dev/archie/landscapeservice/host/responses/HostStatusResponse.java
+++ landscape-service/src/main/java/dev/archie/landscapeservice/host/responses/HostStatusResponse.java
@@ -1,12 +1,10 @@
 package dev.archie.landscapeservice.host.responses;
 
-import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 
 @Data
 @Builder
-@AllArgsConstructor
 public class HostStatusResponse {
 
     private String host;
