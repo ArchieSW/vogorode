5c94992f6c227bb459e3ef539ec106d5590cdda1
ArchieSW <archie-woc@ya.ru>
2023-03-08 09:57:57 +0300

    implement hw4

---
+++ dev/deployment/deploy.sh
@@ -0,0 +1,9 @@
+minikube kubectl -- apply -f secret.yml
+minikube kubectl -- apply -f local-storage.yml
+minikube kubectl -- apply -f postgre-pv.yml
+minikube kubectl -- apply -f postgre-pvc.yml
+minikube kubectl -- apply -f postgre-deployment.yml
+
+minikube kubectl -- apply -f handyman-deployment.yml
+minikube kubectl -- apply -f rancher-deployment.yml
+minikube kubectl -- apply -f landscape-deployment.yml
